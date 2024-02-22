51cdf3edf844f7be8eb2e4919fe18f9f91b2d050
Noob-savvy <151785835+Noob-savvy@users.noreply.github.com>
2024-02-22 10:47:39 +0530

    Add files via upload

---
+++ SavvyMusic/plugins/admins/mute.py
@@ -0,0 +1,28 @@
+
+
+from pyrogram import filters
+from pyrogram.types import Message
+
+from config import BANNED_USERS
+from strings import get_command
+from SavvyMusic import app
+from SavvyMusic.core.call import Savvy
+from SavvyMusic.utils.database import is_muted, mute_on
+from SavvyMusic.utils.decorators import AdminRightsCheck
+
+# Commands
+MUTE_COMMAND = get_command("MUTE_COMMAND")
+
+
+@app.on_message(filters.command(MUTE_COMMAND) & filters.group & ~BANNED_USERS)
+@AdminRightsCheck
+async def mute_admin(cli, message: Message, _, chat_id):
+    if not len(message.command) == 1 or message.reply_to_message:
+        return await message.reply_text(_["general_2"])
+    if await is_muted(chat_id):
+        return await message.reply_text(_["admin_5"], disable_web_page_preview=True)
+    await mute_on(chat_id)
+    await Savvy.mute_stream(chat_id)
+    await message.reply_text(
+        _["admin_6"].format(message.from_user.mention), disable_web_page_preview=True
+    )
---
+++ SavvyMusic/plugins/admins/resume.py
@@ -0,0 +1,27 @@
+
+from pyrogram import filters
+from pyrogram.types import Message
+
+from config import BANNED_USERS
+from strings import get_command
+from SavvyMusic import app
+from SavvyMusic.core.call import Savvy
+from SavvyMusic.utils.database import is_music_playing, music_on
+from SavvyMusic.utils.decorators import AdminRightsCheck
+
+# Commands
+RESUME_COMMAND = get_command("RESUME_COMMAND")
+
+
+@app.on_message(filters.command(RESUME_COMMAND) & filters.group & ~BANNED_USERS)
+@AdminRightsCheck
+async def resume_com(cli, message: Message, _, chat_id):
+    if not len(message.command) == 1:
+        return await message.reply_text(_["general_2"])
+    if await is_music_playing(chat_id):
+        return await message.reply_text(_["admin_3"], disable_web_page_preview=True)
+    await music_on(chat_id)
+    await Savvy.resume_stream(chat_id)
+    await message.reply_text(
+        _["admin_4"].format(message.from_user.mention), disable_web_page_preview=True
+    )
