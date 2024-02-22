fda9a5d3893bc2f25318096de8f9aaceb4e8b72f
Noob-savvy <151785835+Noob-savvy@users.noreply.github.com>
2024-02-22 10:56:09 +0530

    Add files via upload

---
+++ SavvyMusic/utils/inline/song.py
@@ -0,0 +1,27 @@
+
+
+
+from pyrogram.types import InlineKeyboardButton
+
+
+def song_markup(_, vidid):
+    buttons = [
+        [
+            InlineKeyboardButton(
+                text=_["SG_B_2"],
+                callback_data=f"song_helper audio|{vidid}",
+            ),
+            InlineKeyboardButton(
+                text=_["SG_B_3"],
+                callback_data=f"song_helper video|{vidid}",
+            ),
+        ],
+        [
+            InlineKeyboardButton(
+                text="🌻 sᴜᴩᴩᴏʀᴛ 🌻",
+                url="https://t.me/Alexa_Help",
+            ),
+            InlineKeyboardButton(text=_["CLOSE_BUTTON"], callback_data="close"),
+        ],
+    ]
+    return buttons
