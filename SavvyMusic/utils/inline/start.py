a475b4777df24d9158e24f8f5247dbeb3d3a0c20
Noob-savvy <151785835+Noob-savvy@users.noreply.github.com>
2024-03-30 22:43:20 +0530

    Update start.py

--- SavvyMusic/utils/inline/start.py
+++ SavvyMusic/utils/inline/start.py
@@ -49,7 +49,7 @@ def private_panel(_):
         ],
         [InlineKeyboardButton(text=_["S_B_8"], callback_data="settings_back_helper")],
         [
-            InlineKeyboardButton(text=_["S_B_5"], user_id=f"{OWNER_ID}"),
+            InlineKeyboardButton(text=_["S_B_5"], user_id=OWNER_ID),
             InlineKeyboardButton(text=_["S_B_3"], url=f"{SUPPORT_GROUP}"),
         ],
         [
