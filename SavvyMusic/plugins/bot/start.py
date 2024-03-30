340c14ad86f2f61f4922b7d352a71c2536f5082c
Noob-savvy <151785835+Noob-savvy@users.noreply.github.com>
2024-03-30 17:50:35 +0530

    Update start.py

--- SavvyMusic/plugins/bot/start.py
+++ SavvyMusic/plugins/bot/start.py
@@ -176,7 +176,7 @@ async def start_comm(client, message: Message, _):
             OWNER = OWNER_ID[0]
         except:
             OWNER = None
-        out = private_panel(_, app.username, OWNER)
+        out = private_panel(_)
         if config.START_IMG_URL:
             try:
                 await message.reply_photo(
