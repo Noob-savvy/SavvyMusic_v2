79e8615a7cf3ed65acb656dda44c7e6f40b3f1b4
Noob-savvy <151785835+Noob-savvy@users.noreply.github.com>
2024-03-30 11:51:45 +0530

    Update start.py

--- SavvyMusic/plugins/bot/start.py
+++ SavvyMusic/plugins/bot/start.py
@@ -171,40 +171,39 @@ async def start_comm(client, message: Message, _):
                     f"{message.from_user.mention} ᴊᴜsᴛ sᴛᴀʀᴛᴇᴅ ʙᴏᴛ ᴛᴏ ᴄʜᴇᴄᴋ <code>ᴛʀᴀᴄᴋ ɪɴғᴏʀᴍᴀᴛɪᴏɴ</code>\n\n**ᴜsᴇʀ ɪᴅ:** {sender_id}\n**ᴜsᴇʀɴᴀᴍᴇ:** {sender_name}",
                ) 
     else:
-    try:
-        await app.resolve_peer(OWNER_ID[0])
-        OWNER = OWNER_ID[0]
-    except:
-        OWNER = None
-    out = private_panel(_, app.username, OWNER)
-    if config.START_IMG_URL:
         try:
-            await message.reply_photo(
-                photo=config.START_IMG_URL,
-                caption=_["start_2"].format(config.MUSIC_BOT_NAME),
-                reply_markup=InlineKeyboardMarkup(out),
-            )
+            await app.resolve_peer(OWNER_ID[0])
+            OWNER = OWNER_ID[0]
         except:
+            OWNER = None
+        out = private_panel(_, app.username, OWNER)
+        if config.START_IMG_URL:
+            try:
+                await message.reply_photo(
+                    photo=config.START_IMG_URL,
+                    caption=_["start_2"].format(config.MUSIC_BOT_NAME),
+                    reply_markup=InlineKeyboardMarkup(out),
+                )
+            except:
+                await message.reply_photo(
+                    photo=config.START_IMG_URL,
+                    caption=_["start_2"].format(config.MUSIC_BOT_NAME),  # Moved caption before the keyword argument
+                    reply_markup=InlineKeyboardMarkup(out),
+                )
+        else:
             await message.reply_photo(
                 photo=config.START_IMG_URL,
                 caption=_["start_2"].format(config.MUSIC_BOT_NAME),  # Moved caption before the keyword argument
                 reply_markup=InlineKeyboardMarkup(out),
             )
-    else:
-        await message.reply_photo(
-            photo=config.START_IMG_URL,
-            caption=_["start_2"].format(config.MUSIC_BOT_NAME),  # Moved caption before the keyword argument
-            reply_markup=InlineKeyboardMarkup(out),
-        )
-    if await is_on_off(config.LOG):
-        sender_id = message.from_user.id
-        sender_name = message.from_user.first_name
-        return await app.send_message(
-            config.LOG_GROUP_ID,
-            f"{message.from_user.mention} ᴊᴜsᴛ sᴛᴀʀᴛᴇᴅ ʏᴏᴜʀ ʙᴏᴛ.\n\n**ᴜsᴇʀ ɪᴅ:** {sender_id}\n**ᴜsᴇʀɴᴀᴍᴇ:** {sender_name}",
-        )
-
-                
+        if await is_on_off(config.LOG):
+            sender_id = message.from_user.id
+            sender_name = message.from_user.first_name
+            return await app.send_message(
+                config.LOG_GROUP_ID,
+                f"{message.from_user.mention} ᴊᴜsᴛ sᴛᴀʀᴛᴇᴅ ʏᴏᴜʀ ʙᴏᴛ.\n\n**ᴜsᴇʀ ɪᴅ:** {sender_id}\n**ᴜsᴇʀɴᴀᴍᴇ:** {sender_name}",
+            ) 
+               
 
 @app.on_message(
     filters.command(get_command("START_COMMAND")) & filters.group & ~BANNED_USERS
