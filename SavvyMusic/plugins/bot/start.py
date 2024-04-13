85e01e3494add167d97c9f2deca92ac913f080a3
Noob-savvy <151785835+Noob-savvy@users.noreply.github.com>
2024-04-13 11:43:48 +0530

    Update start.py

--- SavvyMusic/plugins/bot/start.py
+++ SavvyMusic/plugins/bot/start.py
@@ -178,31 +178,12 @@ async def start_comm(client, message: Message, _):
                     f"{message.from_user.mention} ᴊᴜsᴛ sᴛᴀʀᴛᴇᴅ ʙᴏᴛ ᴛᴏ ᴄʜᴇᴄᴋ <code>ᴛʀᴀᴄᴋ ɪɴғᴏʀᴍᴀᴛɪᴏɴ</code>\n\n**ᴜsᴇʀ ɪᴅ:** {sender_id}\n**ᴜsᴇʀɴᴀᴍᴇ:** {sender_name}",
                 )
     else:
-        try:
-            await app.resolve_peer(OWNER_ID[0])
-            OWNER = OWNER_ID[0]
-        except:
-            OWNER = None
         out = private_panel(_, app.username, OWNER)
-        if config.START_IMG_URL:
-            try:
-                await message.reply_photo(
-                    photo=random.choice(SAVVY_PIC),
-                    caption=_["start_2"].format(
-                        config.MUSIC_BOT_NAME
-                    ),
-                    reply_markup=InlineKeyboardMarkup(out),
-                )
-            except:
-                await message.reply_text(
-                    _["start_2"].format(config.MUSIC_BOT_NAME),
-                    reply_markup=InlineKeyboardMarkup(out),
-                )
-        else:
-            await message.reply_text(
-                _["start_2"].format(config.MUSIC_BOT_NAME),
-                reply_markup=InlineKeyboardMarkup(out),
-            )
+        await message.reply_video(
+            video="https://telegra.ph//file/5374701ae0678848e9631.mp4",
+            caption=_["start_2"].format(config.MUSIC_BOT_NAME),
+            reply_markup=InlineKeyboardMarkup(out),
+        )
         if await is_on_off(config.LOG):
             sender_id = message.from_user.id
             sender_name = message.from_user.first_name
