2f3dd9479b88e0307e1b33e02bfbbe2edc83f8cd
Noob-savvy <151785835+Noob-savvy@users.noreply.github.com>
2024-04-29 12:09:03 +0530

    Add files via upload

--- SavvyMusic/platforms/__init__.py
+++ SavvyMusic/platforms/__init__.py
@@ -1,1 +1,19 @@
+# Copyright (C) 2024 by Alexa_Help @ Github, < https://github.com/TheTeamAlexa >
+# Subscribe On YT < Jankari Ki Duniya >. All rights reserved. © Alexa © Yukki.
 
+""""
+TheTeamAlexa is a project of Telegram bots with variety of purposes.
+Copyright (c) 2024 -present Team=Alexa <https://github.com/TheTeamAlexa>
+
+This program is free software: you can redistribute it and can modify
+as you want or you can collabe if you have new ideas.
+"""
+
+
+from .Apple import AppleAPI
+from .Carbon import CarbonAPI
+from .Resso import RessoAPI
+from .Soundcloud import SoundAPI
+from .Spotify import SpotifyAPI
+from .Telegram import TeleAPI
+from .Youtube import YouTubeAPI
