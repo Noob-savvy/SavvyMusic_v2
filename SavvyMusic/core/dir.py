520bd2257e3a07cc94863fa6fce97921ae392c9a
Noob-savvy <151785835+Noob-savvy@users.noreply.github.com>
2024-04-29 12:05:24 +0530

    Add files via upload

---
+++ SavvyMusic/core/dir.py
@@ -0,0 +1,33 @@
+
+import os
+import sys
+import logging
+from os import listdir, mkdir
+
+
+def dirr():
+    assets_folder = "assets"
+    downloads_folder = "downloads"
+    cache_folder = "cache"
+
+    if assets_folder not in listdir():
+        logging.warning(
+            f"{assets_folder} Folder not Found. Please clone repository again."
+        )
+        sys.exit()
+
+    for file in os.listdir():
+        if file.endswith(".jpg") or file.endswith(".jpeg"):
+            os.remove(file)
+
+    if downloads_folder not in listdir():
+        mkdir(downloads_folder)
+
+    if cache_folder not in listdir():
+        mkdir(cache_folder)
+
+    logging.info("Directories Updated.")
+
+
+if __name__ == "__main__":
+    dirr()
