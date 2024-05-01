1c1586588fd4f406343c38fd857def63b8e6bc87
Noob-savvy <151785835+Noob-savvy@users.noreply.github.com>
2024-05-01 22:13:05 +0530

    Update __init__.py

--- Backgrounds/__init__.py
+++ Backgrounds/__init__.py
@@ -1,1 +1,9 @@
+import os
+
+thumbs = []
+colors = ["white", "black", "red", "orange", "yellow", "green", "cyan", "azure", "blue", "violet", "magenta", "pink"]
+
+for filename in os.listdir("./Backgrounds"):
+    if filename.endswith("png"):
+        thumbs.append(filename[:-4])
 
