2f6d3675b701c6095c4250cd67490ed631a0188f
Noob-savvy <151785835+Noob-savvy@users.noreply.github.com>
2024-06-11 16:40:36 +0530

    Update callback.py

--- SavvyMusic/plugins/admins/callback.py
+++ SavvyMusic/plugins/admins/callback.py
@@ -218,7 +218,7 @@ async def del_back_playlist(client, CallbackQuery, _):
                 return await CallbackQuery.message.reply_text(_["call_9"])
             theme = await check_theme(chat_id)
             button = telegram_markup(_, chat_id)
-            img = await gen_thumb(videoid, user_id, theme)
+            img = await gen_thumb(videoid, user_id)
             run = await CallbackQuery.message.reply_photo(
                 photo=img,
                 caption=_["stream_1"].format(
@@ -249,7 +249,7 @@ async def del_back_playlist(client, CallbackQuery, _):
                 return await mystic.edit_text(_["call_9"])
             theme = await check_theme(chat_id)
             button = stream_markup(_, videoid, chat_id)
-            img = await gen_thumb(videoid, user_id, theme)
+            img = await gen_thumb(videoid, user_id)
             run = await CallbackQuery.message.reply_photo(
                 photo=img,
                 caption=_["stream_1"].format(
@@ -308,7 +308,7 @@ async def del_back_playlist(client, CallbackQuery, _):
             else:
                 theme = await check_theme(chat_id)
                 button = stream_markup(_, videoid, chat_id)
-                img = await gen_thumb(videoid, user_id, theme)
+                img = await gen_thumb(videoid, user_id)
                 run = await CallbackQuery.message.reply_photo(
                     photo=img,
                     caption=_["stream_1"].format(
