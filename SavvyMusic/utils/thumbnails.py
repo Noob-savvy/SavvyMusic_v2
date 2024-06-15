6ae41aeb13ec2ff9984ae12372ec953aecb99555
Noob-savvy <151785835+Noob-savvy@users.noreply.github.com>
2024-06-15 19:03:51 +0530

    Update thumbnails.py

--- SavvyMusic/utils/thumbnails.py
+++ SavvyMusic/utils/thumbnails.py
@@ -60,125 +60,123 @@ async def gen_thumb(videoid, user_id):
                 result["channel"]["name"]
             except:
                 pass
+            async with aiohttp.ClientSession() as session:
+        async with session.get(thumbnail) as resp:
+            if resp.status == 200:
+                f = await aiofiles.open(f"cache/thumb{videoid}.png", mode="wb")
+                await f.write(await resp.read())
+                await f.close()
 
-        async with aiohttp.ClientSession() as session:
-            async with session.get(thumbnail) as resp:
-                if resp.status == 200:
-                    f = await aiofiles.open(f"cache/thumb{videoid}.png", mode="wb")
-                    await f.write(await resp.read())
-                    await f.close()
+    try:
+        wxy = await app.download_media(
+            (await app.get_users(user_id)).photo.big_file_id,
+            file_name=f"{user_id}.jpg",
+        )
+    except:
+        wxy = await app.download_media(
+            (await app.get_users(app.id)).photo.big_file_id,
+            file_name=f"{app.id}.jpg",
+        )
+    xy = Image.open(wxy)
+    a = Image.new('L', [640, 640], 0)
+    b = ImageDraw.Draw(a)
+    b.pieslice([(0, 0), (640,640)], 0, 360, fill = 255, outline = "white")
+    c = np.array(xy)
+    d = np.array(a)
+    e = np.dstack((c, d))
+    f = Image.fromarray(e)
+    x = f.resize((107, 107))
 
-        try:
-            wxy = await app.download_media(
-                (await app.get_users(user_id)).photo.big_file_id,
-                file_name=f"{user_id}.jpg",
-            )
-        except:
-            wxy = await app.download_media(
-                (await app.get_users(app.id)).photo.big_file_id,
-                file_name=f"{app.id}.jpg",
-            )
-        xy = Image.open(wxy)
-        a = Image.new('L', [640, 640], 0)
-        b = ImageDraw.Draw(a)
-        b.pieslice([(0, 0), (640,640)], 0, 360, fill = 255, outline = "white")
-        c = np.array(xy)
-        d = np.array(a)
-        e = np.dstack((c, d))
-        f = Image.fromarray(e)
-        x = f.resize((107, 107))
+    images = random.choice(thumbs)
+    border = random.choice(colors)
+    youtube = Image.open(f"cache/thumb{videoid}.png")
+    bg = Image.open(f"Backgrounds/{images}.png")
+    image1 = changeImageSize(1280, 720, youtube)
+    image2 = image1.convert("RGBA")
+    background = image2.filter(filter=ImageFilter.BoxBlur(30))
+    enhancer = ImageEnhance.Brightness(background)
+    background = enhancer.enhance(0.6)
 
-        images = random.choice(thumbs)
-        border = random.choice(colors)
-        youtube = Image.open(f"cache/thumb{videoid}.png")
-        bg = Image.open(f"Backgrounds/{images}.png")
-        image1 = changeImageSize(1280, 720, youtube)
-        image2 = image1.convert("RGBA")
-        background = image2.filter(filter=ImageFilter.BoxBlur(30))
-        enhancer = ImageEnhance.Brightness(background)
-        background = enhancer.enhance(0.6)
+    image3 = changeImageSize(1280, 720, bg)
+    image5 = image3.convert("RGBA")
+    Image.alpha_composite(background, image5).save(f"cache/temp{videoid}.png")
 
-        image3 = changeImageSize(1280, 720, bg)
-        image5 = image3.convert("RGBA")
-        Image.alpha_composite(background, image5).save(f"cache/temp{videoid}.png")
+    Xcenter = youtube.width / 2
+    Ycenter = youtube.height / 2
+    x1 = Xcenter - 250
+    y1 = Ycenter - 250
+    x2 = Xcenter + 250
+    y2 = Ycenter + 250
+    logo = youtube.crop((x1, y1, x2, y2))
+    logo.thumbnail((520, 520), Image.LANCZOS)
+    logo.save(f"cache/chop{videoid}.png")
+    if not os.path.isfile(f"cache/cropped{videoid}.png"):
+        im = Image.open(f"cache/chop{videoid}.png").convert("RGBA")
+        add_corners(im)
+        im.save(f"cache/cropped{videoid}.png")
 
-        Xcenter = youtube.width / 2
-        Ycenter = youtube.height / 2
-        x1 = Xcenter - 250
-        y1 = Ycenter - 250
-        x2 = Xcenter + 250
-        y2 = Ycenter + 250
-        logo = youtube.crop((x1, y1, x2, y2))
-        logo.thumbnail((520, 520), Image.LANCZOS)
-        logo.save(f"cache/chop{videoid}.png")
-        if not os.path.isfile(f"cache/cropped{videoid}.png"):
-            im = Image.open(f"cache/chop{videoid}.png").convert("RGBA")
-            add_corners(im)
-            im.save(f"cache/cropped{videoid}.png")
-
-        crop_img = Image.open(f"cache/cropped{videoid}.png")
-        logo = crop_img.convert("RGBA")
-        logo.thumbnail((365, 365), Image.LANCZOS)
-        width = int((1280 - 365) / 2)
-        background = Image.open(f"cache/temp{videoid}.png")
-        background.paste(logo, (width + 2, 138), mask=logo)
-        background.paste(x, (710, 427), mask=x)
-        background.paste(image3, (0, 0), mask=image3)
-        img = ImageOps.expand(background, border=10, fill=f"{border}")
-        draw = ImageDraw.Draw(img)
-        font = ImageFont.truetype("assets/font2.ttf", 45)
-        ImageFont.truetype("assets/font2.ttf", 70)
-        arial = ImageFont.truetype("assets/font2.ttf", 30)
-        ImageFont.truetype("assets/font.ttf", 30)
-        para = textwrap.wrap(title, width=32)
-        try:
+    crop_img = Image.open(f"cache/cropped{videoid}.png")
+    logo = crop_img.convert("RGBA")
+    logo.thumbnail((365, 365), Image.LANCZOS)
+    width = int((1280 - 365) / 2)
+    background = Image.open(f"cache/temp{videoid}.png")
+    background.paste(logo, (width + 2, 138), mask=logo)
+    background.paste(x, (710, 427), mask=x)
+    background.paste(image3, (0, 0), mask=image3)
+    img = ImageOps.expand(background, border=10, fill=f"{border}")
+    draw = ImageDraw.Draw(img)
+    font = ImageFont.truetype("assets/font2.ttf", 45)
+    ImageFont.truetype("assets/font2.ttf", 70)
+    arial = ImageFont.truetype("assets/font2.ttf", 30)
+    ImageFont.truetype("assets/font.ttf", 30)
+    para = textwrap.wrap(title, width=32)
+    try:
+        draw.text(
+            (450, 35),
+            f"PLAYING ON SAVVY",
+            fill="white",
+            stroke_width=1,
+            stroke_fill="white",
+            font=font,
+        )
+        if para[0]:
+            text_w, text_h = draw.textsize(f"{para[0]}", font=font)
             draw.text(
-                (450, 35),
-                f"PLAYING ON SAVVY",
+                ((1280 - text_w) / 2, 560),
+                f"{para[0]}",
                 fill="white",
                 stroke_width=1,
                 stroke_fill="white",
                 font=font,
             )
-            if para[0]:
-                text_w, text_h = draw.textsize(f"{para[0]}", font=font)
-                draw.text(
-                    ((1280 - text_w) / 2, 560),
-                    f"{para[0]}",
-                    fill="white",
-                    stroke_width=1,
-                    stroke_fill="white",
-                    font=font,
-                )
-            if para[1]:
-                text_w, text_h = draw.textsize(f"{para[1]}", font=font)
-                draw.text(
-                    ((1280 - text_w) / 2, 610),
-                    f"{para[1]}",
-                    fill="white",
-                    stroke_width=1,
-                    stroke_fill="white",
-                    font=font,
-                )
-        except:
-            pass
-        text_w, text_h = draw.textsize(f"Duration: {duration} Mins", font=arial)
-        draw.text(
-            ((1280 - text_w) / 2, 665),
-            f"Duration: {duration} Mins",
-            fill="white",
-            font=arial,
-        )
-        try:
-            os.remove(f"cache/thumb{videoid}.png")
-        except:
-            pass
-        img.save(f"cache/{videoid}_{user_id}.png")
-        return f"cache/{videoid}_{user_id}.png"
-    except Exception as e:
-        print(e)
-        return YOUTUBE_IMG_URL
-
+        if para[1]:
+            text_w, text_h = draw.textsize(f"{para[1]}", font=font)
+            draw.text(
+                ((1280 - text_w) / 2, 610),
+                f"{para[1]}",
+                fill="white",
+                stroke_width=1,
+                stroke_fill="white",
+                font=font,
+            )
+    except:
+        pass
+    text_w, text_h = draw.textsize(f"Duration: {duration} Mins", font=arial)
+    draw.text(
+        ((1280 - text_w) / 2, 665),
+        f"Duration: {duration} Mins",
+        fill="white",
+        font=arial,
+    )
+    try:
+        os.remove(f"cache/thumb{videoid}.png")
+    except:
+        pass
+    img.save(f"cache/{videoid}_{user_id}.png")
+    return f"cache/{videoid}_{user_id}.png"
+except Exception as e:
+    print(e)
+    return YOUTUBE_IMG_URL
 
 async def gen_qthumb(videoid, user_id):
     if os.path.isfile(f"cache/que{videoid}_{user_id}.png"):
